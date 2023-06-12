d73b0592c6d7e8cbb64ff853398ae8cb351c2058
Joao DALLA ROSA <joao.dallarosa.ext@louisvuitton.com>
2023-06-12 23:43:58 +0200

    Test design

--- app.vue
+++ app.vue
@@ -1,13 +1,158 @@
 <template>
   <div class="container mx-auto px-6 py-6 text-center h-screen">
-    <div class="h-full border-2 border-black">
-      <img class="max-h-48 block ml-auto mr-auto mb-10 mt-10" src="/images/logo.png" />
-      <div>
-        <a href="www.instagram.com/carvalho_helena">Instagram</a>
+    <nav class="bg-white border-gray-200">
+      <div
+        class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+      >
+        <a href="https://flowbite.com/" class="flex items-center">
+          <img
+            src="@/src/images/logo.png"
+            class="h-20 mr-3"
+            alt="Flowbite Logo"
+          />
+          <!-- <span
+            class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+            >Flowbite</span
+          > -->
+        </a>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span class="sr-only">Open main menu</span>
+          <svg
+            class="w-6 h-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul
+            class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white"
+          >
+            <li>
+              <a
+                href="#"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+                aria-current="page"
+                >Home</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+                >Tattoos</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+                >About</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+                >Contact</a
+              >
+            </li>
+          </ul>
+        </div>
       </div>
-      <div>
-        <a href="www.instagram.com/carvalho_helena">Whatsapp</a>
+    </nav>
+
+    <div
+      role="status"
+      class="space-y-8 animate-pulse md:space-y-0 md:space-x-8 md:flex md:items-center mt-16"
+    >
+      <div
+        class="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700"
+      >
+        <svg
+          class="w-12 h-12 text-gray-200"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 640 512"
+        >
+          <path
+            d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"
+          />
+        </svg>
       </div>
+      <div class="w-full">
+        <div
+          class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"
+        ></div>
+        <div
+          class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[480px] mb-2.5"
+        ></div>
+        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+        <div
+          class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[440px] mb-2.5"
+        ></div>
+        <div
+          class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[460px] mb-2.5"
+        ></div>
+        <div
+          class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"
+        ></div>
+      </div>
+      <span class="sr-only">Loading...</span>
+    </div>
+
+    <div
+      role="status"
+      class="space-y-8 animate-pulse md:space-y-0 md:space-x-8 md:flex md:items-center mt-16"
+    >
+      <div
+        class="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700"
+      >
+        <svg
+          class="w-12 h-12 text-gray-200"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 640 512"
+        >
+          <path
+            d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"
+          />
+        </svg>
+      </div>
+      <div class="w-full">
+        <div
+          class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"
+        ></div>
+        <div
+          class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[480px] mb-2.5"
+        ></div>
+        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+        <div
+          class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[440px] mb-2.5"
+        ></div>
+        <div
+          class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[460px] mb-2.5"
+        ></div>
+        <div
+          class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"
+        ></div>
+      </div>
+      <span class="sr-only">Loading...</span>
     </div>
   </div>
 </template>
@@ -20,10 +165,8 @@ if (process.client) {
   });
 }
 
-const description = ref('HC Tattoo - Tag')
+const description = ref("HC Tattoo - Tag");
 useHead({
-  meta: [
-    { name: 'description', content: description }
-  ],
-})
+  meta: [{ name: "description", content: description }],
+});
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 export default {
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
   nitro: {
     preset: 'vercel-edge',
   },
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+  css: ["@/src/css/tailwind.css"],
 };
