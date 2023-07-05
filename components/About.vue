0340fe9848b1e1c4c067920776f56eab36b333ab
Joao DALLA ROSA <joao.dallarosa.ext@louisvuitton.com>
2023-07-05 22:38:15 +0200

    Testimonials

---
+++ components/About.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+  <div id="about" class="mx-8 border border-black rounded-lg my-10 md:my-20">
+    <div class="grid grid-cols-1 md:grid-cols-2">
+      <div class="flex p-10">
+        <div class="mb-auto mt-auto max-w-lg max-h-[400px]">
+          <h2 class="text-2xl md:text-5xl uppercase text-right">
+            About Helena
+          </h2>
+          <p class="text-lg text-right mt-5">
+            Originally from southern Brazil, now based in Paris, my specialty is
+            creating delicate fine line tattoos that allow clients to express
+            themselves and commemorate meaningful memories. What I love most
+            about my job is connecting with diverse individuals and being a part
+            of their significant moments.
+          </p>
+        </div>
+      </div>
+      <div class="max-h-[200px] md:max-h-[600px]">
+        <img
+          class="w-screen h-full object-cover object-center rounded-b-lg md:rounded-r-lg md:rounded-bl-none"
+          :src="useAssets('about.png')"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ components/Testimonials.vue
@@ -0,0 +1,38 @@
+<script setup>
+const NUMBER_OF_PHOTOS = 9;
+let items = [];
+
+for (let i = 1; i <= NUMBER_OF_PHOTOS; i++) {
+  items = [
+    ...items,
+    {
+      img: useAssets(`testimonial_${i}.jpg`),
+    },
+  ];
+}
+</script>
+
+<template>
+  <div class="my-10" id="testimonials">
+    <h2 class="text-center text-2xl md:text-5xl uppercase">Testimonials</h2>
+    <div class="pt-5 lg:pt-10 px-8" id="home">
+      <masonry-wall
+        :items="items"
+        :column-width="150"
+        :gap="10"
+        :min-columns="2"
+        :max-columns="3"
+      >
+        <template #default="{ item }">
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg"
+              v-lazy="item.img"
+              alt="Tattoo Photo"
+            />
+          </div>
+        </template>
+      </masonry-wall>
+    </div>
+  </div>
+</template>
